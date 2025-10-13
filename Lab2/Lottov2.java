34ef4e8525fe265656c559e2e2140a1ec216e851
ktaborski <ktaborski@student.agh.edu.pl>
2025-10-13 19:45:43 +0200

    Lab2

---
+++ Lab2/Lottov2.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Collections;
+
+public class Lottov2 {
+    public static void main(String[] args) {
+        if (args.length != 6) {
+            System.out.println("Użycie: java Lotto liczba1 liczba2 liczba3 liczba4 liczba5 liczba6");
+            return;
+        }
+
+        ArrayList<Integer> userNumbers = new ArrayList<>();
+        for (String arg : args) {
+            int num = Integer.parseInt(arg); // zakładamy, że to liczba
+            if (num < 1 || num > 49) {
+                System.out.println("Liczby muszą być w zakresie 1–49!");
+                return;
+            }
+            if (!userNumbers.contains(num)) {
+                userNumbers.add(num);
+            } else {
+                System.out.println("Nie podawaj duplikatów!");
+                return;
+            }
+        }
+
+        ArrayList<Integer> lottoNumbers = new ArrayList<>();
+        Random random = new Random();
+
+        while (lottoNumbers.size() < 6) {
+            int num = random.nextInt(49) + 1; // losuje liczbę 1–49
+            if (!lottoNumbers.contains(num)) {
+                lottoNumbers.add(num);
+            }
+        }
+
+        Collections.sort(userNumbers);
+        Collections.sort(lottoNumbers);
+
+        int hits = 0;
+        for (int n : userNumbers) {
+            if (lottoNumbers.contains(n)) {
+                hits++;
+            }
+        }
+
+        System.out.println("Twoje typy: " + userNumbers);
+        System.out.println("Wylosowane liczby: " + lottoNumbers);
+        System.out.println("Liczba trafień: " + hits);
+    }
+}
+
